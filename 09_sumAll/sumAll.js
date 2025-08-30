47e1d289c4684ed2c72070d32c8f7acfa3e1bc44
Patrik <patrik.vg@icloud.com>
2025-08-30 18:09:34 +0200

    09 finished

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+ if (num1 < 0 || num2 < 0) {
+    return "ERROR";
+  } 
 
+  let sum = 0;
+
+  for (let i = num1; i <= num2; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
