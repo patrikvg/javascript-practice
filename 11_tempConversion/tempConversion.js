ca7dd0274a5c0579f2566b5050c1cd3d5c7dccf2
Patrik <patrik.vg@icloud.com>
2025-09-02 22:29:03 +0200

    task 11 finished

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let help = 0;
+  help = (fahrenheit - 32) / 1.8;
+
+  let rounded = Math.round(help * 10) / 10;
+  return rounded; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let help = 0;
+  help = (celsius * 1.8) + 32; 
+  let rounded = Math.round(help * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
