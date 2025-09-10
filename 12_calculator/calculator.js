d40592e63b3f87602c433ab2088020092d4f75c4
Patrik <patrik.vg@icloud.com>
2025-09-10 16:49:51 +0200

    Task 12 completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+
+return arr.reduce((zws, num) => zws + num, 0); // 1.Wert = Zwischensumme 2.Wert = atuelles Element letzte wert also die 0 = Startwert 
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
 
+return arr.reduce((zws, num) => zws * num, 1); 
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0) {
+    return 1;
+  } 
+
+  let solution = 1;
+
+  for (let i = 1; i <= num; i++) {
+    solution *= i;  
+  }
+
+  return solution; 
 };
 
 // Do not edit below this line
