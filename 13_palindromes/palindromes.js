263a2bccbf847211d562b9d0708ba1a778775e5e
Patrik <patrik.vg@icloud.com>
2025-09-10 23:14:00 +0200

    Task 13 completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const normalized = string.toLowerCase().replace(/[^a-z0-9]+/g, '');
+  let reversed = normalized.split('').reverse().join('');
+  
+  if (normalized === reversed) {
+    return true;
+  }
+  return false;
 };
 
 // Do not edit below this line
